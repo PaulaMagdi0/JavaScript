60016ef0f6de8569792ed87474adb6f64efab4a5
“Paula <paulamagdy665@gmail.com>
2024-12-21 16:02:31 +0200

    Change prompt to confirm

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-// Question one
-// --------------
+// // Question one
+// // --------------
 
-// function sort(x, y, z) {
-//   if (x > y && y > z) {
-//     alert(x + ", " + y + ", " + z);
-//   } else if (y > x && x > z) {
-//     alert(y + ", " + x + ", " + z);
-//   } else if (z > x && x > y) {
-//     alert(z + ", " + x + ", " + y);
-//   } else {
-//     alert(z + ", " + y + ", " + x);
-//   }
-// }
-// sort(5,10,1)
+// // function sort(x, y, z) {
+// //   if (x > y && y > z) {
+// //     alert(x + ", " + y + ", " + z);
+// //   } else if (y > x && x > z) {
+// //     alert(y + ", " + x + ", " + z);
+// //   } else if (z > x && x > y) {
+// //     alert(z + ", " + x + ", " + y);
+// //   } else {
+// //     alert(z + ", " + y + ", " + x);
+// //   }
+// // }
+// // sort(5,10,1)
 
-// Question two
-// --------------
+// // Question two
+// // --------------
 
 // function fizzBuzz() {
 //   for(var i=1;i<=100;i++){
@@ -32,8 +32,8 @@
 // }
 // fizzBuzz();
 
-// Question three
-// --------------
+// // Question three
+// // --------------
 
 // function sumition() {
 //   var sum=0;
@@ -46,33 +46,33 @@
 // }
 // sumition();
 
-// Question four
-// --------------
+// // Question four
+// // --------------
 
-// var count = 0;
+var count = 0;
 
-// function guessGame() {
-//   var userGuess = prompt("Guess a number between 1 and 10:");
-//   userGuess = parseInt(userGuess, 10);
-//   if (isNaN(userGuess) || userGuess < 1 || userGuess > 10) {
-//       alert("Please enter a valid number between 1 and 10.");
-//       guessGame(); 
-//       return;
-//   }
-//   count++;
-//   if (userGuess === 5) { // or use Math.floor(Math.random() * 10) + 1
-//       alert("Good Work!");
-//   } else {
-//       if (count < 3) {
-//         var tryAgain = prompt("Wrong guess, wanna try again (y,n)");
-//         if (tryAgain == "y") {
-//             guessGame();
-//         } else {
-//             alert("Thanks for playing!"); 
-//         }
-//       } else {
-//           alert("Game over!");
-//       }
-//   }
-// }
-// guessGame();
+function guessGame() {
+  var userGuess = prompt("Guess a number between 1 and 10:");
+  userGuess = parseInt(userGuess, 10);
+  if (isNaN(userGuess) || userGuess < 1 || userGuess > 10) {
+      alert("Please enter a valid number between 1 and 10.");
+      guessGame(); 
+      return;
+  }
+  count++;
+  if (userGuess === 5) { // or use Math.floor(Math.random() * 10) + 1
+      alert("Good Work!");
+  } else {
+      if (count < 3) {
+        var tryAgain = confirm("Wrong guess, wanna try again (y,n)");
+        if (tryAgain == "y") {
+            guessGame();
+        } else {
+            alert("Thanks for playing!"); 
+        }
+      } else {
+          alert("Game over!");
+      }
+  }
+}
+guessGame();
